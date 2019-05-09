28686a078895cafd9ef355d4faffe695985a2690
zpfz <zpzf@seeyoz.cn>
2019-05-10 00:18:56 +0800

    Signed-off-by: zpfz

---
+++ static/lang/en_US.js
@@ -0,0 +1,18 @@
+export default {
+	nav: {
+		home: 'Home',
+		components: 'Project',
+		components_1: 'DeAndroid',
+		documents: 'Documents',
+		faqs: 'FAQs',
+		community: 'Community',
+		tips: 'Click me to toggle language',
+		text: 'Language'
+	},
+	panel: {
+		headline_1: 'SEEYOZ',
+		headline_2: 'Official Open Source Project',
+		subtitle: 'If you have an idea, I have an idea, exchange with each other, we each have two kinds of ideas, even more than two kinds of ideas.',
+		btntext: 'Go head'
+	}
+}
---
+++ static/lang/zh_CN.js
@@ -0,0 +1,18 @@
+export default {
+	nav: {
+		home: '西柚子官网',
+		components: '开源项目',
+		components_1: 'DeAndroid',
+		documents: '在线文档',
+		faqs: '疑难解答',
+		community: '交流社区',
+		tips: '点我切换语言',
+		text: '语言'
+	},
+	panel: {
+		headline_1: '西柚子',
+		headline_2: '官方开源项目',
+		subtitle: '如果你有一种思想，我有一种思想，彼此交换，我们每个人就有了两种思想，甚至多于两种思想。',
+		btntext: '开始使用'
+	}
+}
