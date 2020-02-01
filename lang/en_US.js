707ab435f0b9866e6d980e69b9ee7747a2a09ab1
左撇峰子 <zpfz@seeyoz.cn>
2020-02-01 23:47:49 +0800

    no message

---
+++ lang/en_US.js
@@ -0,0 +1,21 @@
+export default {
+	nav: {
+		home: 'Home',
+		project: 'Project',
+		project_1: 'DeAndroid',
+		scaffold: 'Scaffold',
+		scaffold_1: 'ev-cli3',
+		batch:'Batch',
+		batch_1:'createShortcut',
+		documents: 'Documents',
+		community: 'Community',
+		tips: 'Click me to toggle language',
+		text: 'Language'
+	},
+	panel: {
+		headline_1: 'SEEYOZ',
+		headline_2: 'Official Open Source Project',
+		subtitle: 'If you have an idea, I have an idea, exchange with each other, we each have two kinds of ideas, even more than two kinds of ideas.',
+		btntext: 'Getting Started'
+	}
+}
